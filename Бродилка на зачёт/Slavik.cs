727e6860a4bdc616d896764fd9768c8eddec9042
FreecouS_BG <63102871+Slavamk3dell@users.noreply.github.com>
2023-09-25 11:45:12 +0300

    add slavick

---
+++ Бродилка на зачёт/Slavik.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Бродилка_на_зачёт
+{
+    internal class Slavik
+    {
+    }
+}

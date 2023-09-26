b06f889a6ba271a053d133e7e607a96416298e2b
Andreev A <135585722+VeryLazyCoder@users.noreply.github.com>
2023-09-26 15:44:58 +0300

    remove todo comment

--- Бродилка на зачёт/PlayerInfo.cs
+++ Бродилка на зачёт/PlayerInfo.cs
@@ -2,7 +2,7 @@
 
 namespace HodimBrodim
 {
-    public class PlayerInfo//todo: вынести имя, счёт и время в отдельную структуру
+    public class PlayerInfo
     {
         private string _name;
         private int _score;
